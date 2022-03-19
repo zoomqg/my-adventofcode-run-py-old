d8d12ac0ef82605b094395a3db2b60a19b76dddf
zoomqg <pavlovs.misha@gmail.com>
2022-03-19 16:21:48 +0200

    Day_5

--- Day_5/Part_2/script.py
+++ Day_5/Part_2/script.py
@@ -18,7 +18,7 @@ data_list = [x.split(',') for x in data_list]
 data_list = np.array(data_list, int)
 data_list = data_list.reshape(len(data_list), 2, 2)
 
-print(data_list)
+# print(data_list)
 
 # making blank diagram with zeros
 find_max = np.amax(data_list, axis=0)
@@ -49,6 +49,31 @@ for i in range(0, len(data_list)):
         finish_point += 1
         for column in range(start_point, finish_point):
             diagram[row][column] += 1
+    else:
+        # preparing all axes
+        start_row = data_list[i][0][1]
+        finish_row = data_list[i][1][1]
+        start_column = data_list[i][0][0]
+        finish_column = data_list[i][1][0]
+        # i can't make array with reversed range, this is the first solution that came to my mind
+        if start_row > finish_row:
+            start_row, finish_row = finish_row, start_row
+            y_trajectory = np.arange(start_row, finish_row + 1)
+            y_trajectory = np.flip(y_trajectory, 0)
+        else:
+            y_trajectory = np.arange(start_row, finish_row + 1)
+
+        if start_column > finish_column:
+            start_column, finish_column = finish_column, start_column
+            x_trajectory = np.arange(start_column, finish_column + 1)
+            x_trajectory = np.flip(x_trajectory, 0)
+        else:
+            x_trajectory = np.arange(start_column, finish_column + 1)      
+
+        # x and y axes have same lenght, so i can use any length for range
+        for x in range(0, len(x_trajectory)):
+            diagram[y_trajectory[x]][x_trajectory[x]] += 1
+            
 
 
 #print(diagram)
