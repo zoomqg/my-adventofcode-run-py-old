7c257fb55830a2d4648a263a5034fd93034f3ae0
zoomqg <pavlovs.misha@gmail.com>
2022-03-19 12:32:24 +0200

    Part_2 W.I.P

--- Day_5/Part_1/script.py
+++ Day_5/Part_1/script.py
@@ -12,7 +12,7 @@ data_list = data.split("\n")
 # closing the file
 file.close()
 
-#removing all unnecessary chars and creating a numpy array
+# removing all unnecessary chars and creating a numpy array
 data_list = [x.replace(' -> ', ',') for x in data_list]
 data_list = [x.split(',') for x in data_list]
 data_list = np.array(data_list, int)
@@ -20,14 +20,14 @@ data_list = data_list.reshape(len(data_list), 2, 2)
 
 print(data_list)
 
-#making blank diagram with zeros
+# making blank diagram with zeros
 find_max = np.amax(data_list, axis=0)
 max_x = max(find_max[0][0], find_max[1][0]) + 1
 max_y = max(find_max[0][1], find_max[1][1]) + 1
 diagram = np.zeros((max_y, max_x))
 
 for i in range(0, len(data_list)):
-    #cover y axis (checking if x axis is same in both lines)
+    # cover y axis (checking if x axis is same in both lines)
     if data_list[i][0][0] == data_list[i][1][0]:
         column = data_list[i][0][0]
         start_point = data_list[i][0][1]
